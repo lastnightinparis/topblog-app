c70f846b707993a07cc8e8c916ce4709312b04df
Kirill Tankovskiy <tankovskiyko@gmail.com>
2023-08-25 00:37:10 +0300

    feat: initial commit take 1

---
+++ src/main/java/org/squad/topblogapp/TopblogAppApplication.java
@@ -0,0 +1,13 @@
+package org.squad.topblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopblogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopblogAppApplication.class, args);
+	}
+
+}
