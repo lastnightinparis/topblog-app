18078a44ac772e492511ccb1b99369ee1f1f3ef3
Kirill Tankovskiy <tankovskiyko@gmail.com>
2023-08-26 18:23:50 +0300

    feat: add remote api config - take 1

--- src/main/java/org/squad/topblogapp/config/properties/RemoteApiConfiguration.java
+++ src/main/java/org/squad/topblogapp/config/properties/RemoteApiConfiguration.java
@@ -1,13 +1,19 @@
 package org.squad.topblogapp.config.properties;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import lombok.Setter;
+
 @Configuration
+@ConfigurationProperties(prefix = "remote.api")
+@Setter
 public class RemoteApiConfiguration {
+    private String client;
     @Bean
     public WebClient remoteApiClient() {
-        return WebClient.create("http://51.250.22.177:8080");
+        return WebClient.create(client);
     }
 }
